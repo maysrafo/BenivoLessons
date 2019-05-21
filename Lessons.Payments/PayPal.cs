0390744638c3da76a58fc92f2d17441bf9d267af
Rafayel Maysuryan <rafael@benivo.com>
2019-05-21 20:26:06 +0400

    First commit

---
+++ Lessons.Payments/PayPal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons.Payments
+{
+    public class PayPal
+    {
+       public static void Pay(int amount)
+        {
+            Console.WriteLine("Paid " + amount + " successfully with PayPal");
+            Console.WriteLine($"Paid {amount} successfully with PayPal");
+        }
+    }
+}
---
+++ Lessons/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Lessons.Payments;
+
+namespace Lessons
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World");
+
+            PayPal.Pay(100);
+            PayPal.Pay(50);
+
+        }
+    }
+}

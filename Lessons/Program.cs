82f8979eff309e1214136487f8f286e0018c2c08
Rafayel Maysuryan <rafael@benivo.com>
2019-05-30 18:47:21 +0400

    Forth commit

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using Lessons.Payments;
 using Lessons.Payouts;
 
 namespace Lessons
@@ -8,15 +7,27 @@ namespace Lessons
     {
         static void Main()
         {
-            Console.WriteLine("Hello World");
+            /*
+            bool isMarried = false;  // 1bit
+            string name = "Armen"; // 2 bytes for each character - 10 byte = 80 bit
+            byte color = 255; // 1 byte == 8 bit
+            int id = 412234; //4 byte == 32 bit (int32)
+            short id1 = 12315; // 2 byte == 16 bit (int16)
+            long id2 = 1231516143456465454; // 8 byte == 64 bit (int64)
+            float amount = 4.34f; // 4 byte
+            double amount1 = 4.34; // 8 byte
+            decimal amount2 = 3.123m; // 16 byte
+            char symbol = 'a'; // 2 byte == 16 bit
 
-            PayPal.Pay(100);
-            PayPal.Pay(50);
-            PayPal.Pay(200);
+            Console.WriteLine(isMarried.ToString());     
+            */
 
-            Payout.Withdraw(150);
-            Payout.Withdraw(250);
-            Payout.Withdraw(350);
+            bool isRafayel = false;
+            bool isArmenian = true;
+
+            Console.WriteLine(isRafayel.ToString().ToLower());
+            Console.WriteLine(isArmenian.ToString().ToLower());
+
         }
     }
 }

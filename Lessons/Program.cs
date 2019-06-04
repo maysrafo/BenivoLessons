2d9cb95eb333bafcb2c4a1435c611fb73376ef63
Rafayel Maysuryan <rafael@benivo.com>
2019-06-04 21:06:12 +0400

    My First Application

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 using Lessons.Payouts;
 
 namespace Lessons
@@ -7,26 +8,120 @@ namespace Lessons
     {
         static void Main()
         {
-            /*
-            bool isMarried = false;  // 1bit
-            string name = "Armen"; // 2 bytes for each character - 10 byte = 80 bit
-            byte color = 255; // 1 byte == 8 bit
-            int id = 412234; //4 byte == 32 bit (int32)
-            short id1 = 12315; // 2 byte == 16 bit (int16)
-            long id2 = 1231516143456465454; // 8 byte == 64 bit (int64)
-            float amount = 4.34f; // 4 byte
-            double amount1 = 4.34; // 8 byte
-            decimal amount2 = 3.123m; // 16 byte
-            char symbol = 'a'; // 2 byte == 16 bit
+            //Introduction
+            Console.WriteLine("Welcome to Benivo, to continue please Enter");
+            Console.ReadLine();
+            Console.Clear();
 
-            Console.WriteLine(isMarried.ToString());     
-            */
+            Console.WriteLine("Please answer some question to have you onboard");
 
-            bool isRafayel = false;
-            bool isArmenian = true;
+            //Check User age
+            Console.WriteLine("How old are you?");
+            string userAge = Console.ReadLine();
 
-            Console.WriteLine(isRafayel.ToString().ToLower());
-            Console.WriteLine(isArmenian.ToString().ToLower());
+            int age;
+            age = int.Parse(userAge);
+
+            if (age > 18 || age == 18)
+            {
+                Console.WriteLine("You are allowed to continue your sign up process");
+
+                //email
+                Console.WriteLine("Please enter your email :");
+                
+                string userEmail = Console.ReadLine();
+            
+
+                //password
+                Console.WriteLine("Please enter your password");
+                
+                string userPassword = Console.ReadLine();
+
+                //firstname
+                Console.WriteLine("Please Enter your FirstName");
+                string firstName = Console.ReadLine();
+
+                //lastname
+                Console.WriteLine("Please Enter your LastName");
+                string lastname = Console.ReadLine();
+
+                //day of birthday
+                Console.WriteLine("Please Enter your Day of birthday");
+                string day = Console.ReadLine();
+
+                int birthday_day;
+
+                birthday_day = int.Parse(day);
+
+
+                //month of birthday
+                Console.WriteLine("Please Enter Month of your birthday");
+                string month = Console.ReadLine();
+
+                int birthday_month;
+
+                birthday_month = int.Parse(month);
+
+
+
+                //Year of birthday
+                Console.WriteLine("Please Enter Year of your birthday");
+                string year = Console.ReadLine();
+
+                int birthday_year;
+
+                birthday_year = int.Parse(year);
+
+
+                //Converting user inputs to date
+
+                string bornDate = $"{year}-{month}-{day}";
+
+                DateTime dt;
+                //dt = DateTime.ParseExact(bornDate, "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                dt = new DateTime(birthday_year, birthday_month, birthday_day);
+
+
+
+                //Return Output
+                string output;
+                Console.WriteLine($"Hi {firstName} {lastname}, you are registered with account {userEmail}. Your have born on {dt}");
+
+                DateTime today = DateTime.Now;
+
+                //if (dt.Date.Year > today.AddYears(-age).Year && dt.Date.Month > today.AddYears(-age).Month && dt.Date.Day > today.AddYears(-age).Day)
+                ////{
+                //    Console.WriteLine("You did not lie :)");
+                //}
+                //else
+                //{
+                //    Console.WriteLine("You are sharper");
+                //}
+
+
+
+                TimeSpan dayCount = (DateTime.Now - dt);
+                int totalYear = (int)dayCount.TotalDays / 365;
+
+                if (totalYear == age)
+                {
+                    Console.WriteLine("You are registered successfully");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("You are sharper");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you are not allowed to Sign up");
+          
+            }
+
+            Console.ReadKey();
+
 
         }
     }

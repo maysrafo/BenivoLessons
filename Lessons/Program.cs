354e519139f02c702c1a6af6befaded08b2706e4
Rafayel Maysuryan <rafael@benivo.com>
2019-06-06 18:15:33 +0400

    My Signup Application : Version 3

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -9,8 +9,11 @@ namespace Lessons
     {
         static void Main()
         {
+            ConsoleColor foreground = Console.ForegroundColor;
+
+
             //Introduction
-            Console.WriteLine("Please answer some question to have you onboard, please Enter to continue");
+            Console.WriteLine("Please answer some questions to have you onboard, please Enter to continue");
             Console.ReadLine();
             Console.Clear();
 
@@ -25,40 +28,62 @@ namespace Lessons
             //Validating user age
             if (age < 18)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Sorry, you are not allowed to Sign up");
             }
             else
             {
-
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("You are allowed to continue\n");
 
                 //Email Address input
+                Console.ResetColor();
                 Console.WriteLine("Please enter your email :");
-                string emailInput = EmailAddress();
+                string firstEmailInput = EmailAddress();
+                Console.Clear();
 
                 //Validation email Address
                 Regex regexEmail = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
-                Match matchEmail = regexEmail.Match(emailInput);
+                Match matchEmail = regexEmail.Match(firstEmailInput);
                 if (matchEmail.Success)
                 {
-                    Console.WriteLine(emailInput + "format is correct");
-                    Console.Clear();
-                
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your email format is correct");
+
                 }
                 else
                 {
-                    Console.WriteLine(emailInput + "is incorrect, try again");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" Your Email format is incorrect, try again");
+
+                    Console.ResetColor();
+                    string secondEmailInput = EmailAddress();
                     Console.Clear();
-                    //EmailAddress();
-                    //Console.WriteLine(emailInput + "is again incorrect, try again the last time :(");
-                    //EmailAddress();
-                    //Console.WriteLine("Sorry you are not allowed to Sign up");
+                    Match matchEmail1 = regexEmail.Match(secondEmailInput);
+
+                    if (matchEmail1.Success)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Your Email format is correct\n");
+
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your email format is incorrect\n");
+
+                    }
+
                 }
 
 
+
+
                 //password
+                Console.ResetColor();
                 Console.WriteLine("Please enter your password. Please note It must contain at least a number, one upper case letter and 8 characters long");
                 string userPassword = UserPassword();
+                Console.Clear();
 
                 //Password Validation
                 var hasNumber = new Regex(@"[0-9]+");
@@ -69,62 +94,90 @@ namespace Lessons
 
                 if (isValidated)
                 {
-                    Console.WriteLine("Your password format is correct");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your password format is correct, please Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
                 else
                 {
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("Please Enter a valid password");
-                    //UserPassword();
-                    //Console.WriteLine("Please Enter a valid password last time");
-                    //UserPassword();
-                    //Console.WriteLine("Oooops, you are not allowed to Enter password, Try again Later");
-                    //Console.Clear();
+                    Console.ResetColor();
+                    string passResult = UserPassword();
+                    Console.Clear();
+                    var hasNumber1 = new Regex(@"[0-9]+");
+                    var hasUpperChar1 = new Regex(@"[A-Z]+");
+                    var hasMinimum8Chars1 = new Regex(@".{8,}");
+
+                    bool isValidated1 = hasNumber1.IsMatch(passResult) && hasUpperChar1.IsMatch(passResult) && hasMinimum8Chars1.IsMatch(passResult);
+
+                    if (isValidated1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("Your password format is correct now\n ");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Your password is again incorrect, you are not allowed to continue, please Enter");
+                        Console.ReadLine();
+                        Console.Clear();
+                    }
+
+
+
+
                 }
 
                 //firstname
+                Console.ResetColor();
                 Console.WriteLine("Please Enter your FirstName");
                 string firstName = UserFirstName();
 
                 //firstname validation
-                if (!Regex.Match(firstName, "^[A-Z][a-zA-Z]*$").Success)
+                if (Regex.Match(firstName, "^[A-Z][a-zA-Z]*$").Success)
                 {
-                    Console.WriteLine("Please do not use numbers in your name");
-                    //UserFirstName();
-                    //Console.WriteLine("Please do not use numbers in your name, this is last opprtunity");
-                    //UserFirstName();
-                    //Console.WriteLine("You are not allowed to continue");
-                    //Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please do not use numbers in your name next time :)");
 
                 }
                 else
                 {
-                    Console.WriteLine("Your First Name format is correct");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your First Name format is correct, Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
 
-                //lastname
+                //lastname 
+                Console.ResetColor();
                 Console.WriteLine("Please Enter your LastName");
                 string lastname = UserLastName();
 
                 //lastname validation
-                if (!Regex.Match(lastname, "^[A-Z][a-zA-Z]*$").Success)
+                if (Regex.Match(lastname, "^[A-Z][a-zA-Z]*$").Success)
                 {
-                    Console.WriteLine("Please do not use numbers in your name");
-                    //UserLastName();
-                    //Console.WriteLine("Please do not use numbers in your name, try again");
-                    //UserLastName();
-                    //Console.WriteLine("You are not allowed to continue");
-                    //Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Please do not use numbers in your name next time :)");
 
                 }
                 else
                 {
-                    Console.WriteLine("Your First Name format is correct");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Your Last Name format is correct, Enter to continue");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
 
 
                 //day of birthday
+                Console.ResetColor();
                 Console.WriteLine("Please Enter your Day of birthday");
                 string day = Console.ReadLine();
+                Console.Clear();
 
                 int birthday_day;
 
@@ -134,6 +187,7 @@ namespace Lessons
                 //month of birthday
                 Console.WriteLine("Please Enter Month of your birthday");
                 string month = Console.ReadLine();
+                Console.Clear();
 
                 int birthday_month;
 
@@ -144,6 +198,7 @@ namespace Lessons
                 //Year of birthday
                 Console.WriteLine("Please Enter Year of your birthday");
                 string year = Console.ReadLine();
+                Console.Clear();
 
                 int birthday_year;
 
@@ -161,8 +216,8 @@ namespace Lessons
 
 
                 //Return Output
-                string output;
-                Console.WriteLine($"Hi {firstName} {lastname}, you are registered with account {emailInput}. Your have born on {dt}");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"Hi {firstName} {lastname}, you are registered with account {firstEmailInput}. Your born date  is {dt}");
 
                 DateTime today = DateTime.Now;
 
@@ -177,7 +232,8 @@ namespace Lessons
                 }
                 else
                 {
-                    Console.WriteLine("You are sharper");
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"\n\nBut a quick note to you, You are sharper you have deceived us that you are {age} years old");
 
                 }
 
@@ -185,7 +241,7 @@ namespace Lessons
 
             }
 
-            
+
         }
 
         //Function to ask user to input Email Address

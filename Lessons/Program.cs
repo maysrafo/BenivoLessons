644cce2cf8f22a3f7492d37976adc9a24dcdef5d
Rafayel Maysuryan <rafael@benivo.com>
2019-06-06 18:09:56 +0400

    My Signup application version 2

--- Lessons/Program.cs
+++ Lessons/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using Lessons.Payouts;
+using System.Regex;
 
 namespace Lessons
 {
@@ -9,41 +10,117 @@ namespace Lessons
         static void Main()
         {
             //Introduction
-            Console.WriteLine("Welcome to Benivo, to continue please Enter");
+            Console.WriteLine("Please answer some question to have you onboard, please Enter to continue");
             Console.ReadLine();
             Console.Clear();
 
-            Console.WriteLine("Please answer some question to have you onboard");
-
-            //Check User age
+            //Asking user age
             Console.WriteLine("How old are you?");
             string userAge = Console.ReadLine();
+            Console.Clear();
 
             int age;
             age = int.Parse(userAge);
 
-            if (age > 18 || age == 18)
+            //Validating user age
+            if (age < 18)
             {
-                Console.WriteLine("You are allowed to continue your sign up process");
+                Console.WriteLine("Sorry, you are not allowed to Sign up");
+            }
+            else
+            {
 
-                //email
+                Console.WriteLine("You are allowed to continue\n");
+
+                //Email Address input
                 Console.WriteLine("Please enter your email :");
+                string emailInput = EmailAddress();
+
+                //Validation email Address
+                Regex regexEmail = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match matchEmail = regexEmail.Match(emailInput);
+                if (matchEmail.Success)
+                {
+                    Console.WriteLine(emailInput + "format is correct");
+                    Console.Clear();
                 
-                string userEmail = Console.ReadLine();
-            
+                }
+                else
+                {
+                    Console.WriteLine(emailInput + "is incorrect, try again");
+                    Console.Clear();
+                    //EmailAddress();
+                    //Console.WriteLine(emailInput + "is again incorrect, try again the last time :(");
+                    //EmailAddress();
+                    //Console.WriteLine("Sorry you are not allowed to Sign up");
+                }
+
 
                 //password
-                Console.WriteLine("Please enter your password");
-                
-                string userPassword = Console.ReadLine();
+                Console.WriteLine("Please enter your password. Please note It must contain at least a number, one upper case letter and 8 characters long");
+                string userPassword = UserPassword();
+
+                //Password Validation
+                var hasNumber = new Regex(@"[0-9]+");
+                var hasUpperChar = new Regex(@"[A-Z]+");
+                var hasMinimum8Chars = new Regex(@".{8,}");
+
+                bool isValidated = hasNumber.IsMatch(userPassword) && hasUpperChar.IsMatch(userPassword) && hasMinimum8Chars.IsMatch(userPassword);
+
+                if (isValidated)
+                {
+                    Console.WriteLine("Your password format is correct");
+                }
+                else
+                {
+                    Console.WriteLine("Please Enter a valid password");
+                    //UserPassword();
+                    //Console.WriteLine("Please Enter a valid password last time");
+                    //UserPassword();
+                    //Console.WriteLine("Oooops, you are not allowed to Enter password, Try again Later");
+                    //Console.Clear();
+                }
 
                 //firstname
                 Console.WriteLine("Please Enter your FirstName");
-                string firstName = Console.ReadLine();
+                string firstName = UserFirstName();
+
+                //firstname validation
+                if (!Regex.Match(firstName, "^[A-Z][a-zA-Z]*$").Success)
+                {
+                    Console.WriteLine("Please do not use numbers in your name");
+                    //UserFirstName();
+                    //Console.WriteLine("Please do not use numbers in your name, this is last opprtunity");
+                    //UserFirstName();
+                    //Console.WriteLine("You are not allowed to continue");
+                    //Console.Clear();
+
+                }
+                else
+                {
+                    Console.WriteLine("Your First Name format is correct");
+                }
 
                 //lastname
                 Console.WriteLine("Please Enter your LastName");
-                string lastname = Console.ReadLine();
+                string lastname = UserLastName();
+
+                //lastname validation
+                if (!Regex.Match(lastname, "^[A-Z][a-zA-Z]*$").Success)
+                {
+                    Console.WriteLine("Please do not use numbers in your name");
+                    //UserLastName();
+                    //Console.WriteLine("Please do not use numbers in your name, try again");
+                    //UserLastName();
+                    //Console.WriteLine("You are not allowed to continue");
+                    //Console.Clear();
+
+                }
+                else
+                {
+                    Console.WriteLine("Your First Name format is correct");
+                }
+
 
                 //day of birthday
                 Console.WriteLine("Please Enter your Day of birthday");
@@ -85,19 +162,9 @@ namespace Lessons
 
                 //Return Output
                 string output;
-                Console.WriteLine($"Hi {firstName} {lastname}, you are registered with account {userEmail}. Your have born on {dt}");
+                Console.WriteLine($"Hi {firstName} {lastname}, you are registered with account {emailInput}. Your have born on {dt}");
 
                 DateTime today = DateTime.Now;
-
-                //if (dt.Date.Year > today.AddYears(-age).Year && dt.Date.Month > today.AddYears(-age).Month && dt.Date.Day > today.AddYears(-age).Day)
-                ////{
-                //    Console.WriteLine("You did not lie :)");
-                //}
-                //else
-                //{
-                //    Console.WriteLine("You are sharper");
-                //}
-
 
 
                 TimeSpan dayCount = (DateTime.Now - dt);
@@ -106,23 +173,48 @@ namespace Lessons
                 if (totalYear == age)
                 {
                     Console.WriteLine("You are registered successfully");
-                    
+
                 }
                 else
                 {
                     Console.WriteLine("You are sharper");
 
                 }
-            }
-            else
-            {
-                Console.WriteLine("Sorry, you are not allowed to Sign up");
-          
+
+                Console.ReadKey();
+
             }
 
-            Console.ReadKey();
+            
+        }
 
+        //Function to ask user to input Email Address
+        public static string EmailAddress()
+        {
+            string userEmail = Console.ReadLine();
+            return userEmail;
 
+        }
+
+        //Function to ask user to input Password
+        public static string UserPassword()
+        {
+            string password = Console.ReadLine();
+            return password;
+        }
+
+        //Function to ask user to input First name
+        public static string UserFirstName()
+        {
+            string inputFirstName = Console.ReadLine();
+            return inputFirstName;
+        }
+
+        //Function to ask user to input Last name
+        public static string UserLastName()
+        {
+            string inputLastName = Console.ReadLine();
+            return inputLastName;
         }
     }
 }
